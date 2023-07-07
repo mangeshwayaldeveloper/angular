414e3e0827156b4e83cf70a3f1e176e647eb621f
My name <me@example.com>
2023-07-07 12:12:00 +0530

    task first completed

--- Task1/taskProject/src/app/app.component.ts
+++ Task1/taskProject/src/app/app.component.ts
@@ -9,32 +9,8 @@ import {style} from "@angular/animations";
 export class AppComponent {
   title = 'taskProject';
   postTitleText: string = "";
-  showTitle: string = "";
   postDetailsText: string = "";
-  showDetails: string = "";
   imageUrl: string = "";
-  showImage: string = "";
   urlText: string = "";
-  showUrl: string = "";
-  bgColor: string = "yellow";
-  showBgColor:string="";
-  OnKey() {
-    this.showTitle = this.postTitleText;
-  }
-
-  OnKey2() {
-    this.showDetails = this.postDetailsText;
-  }
-
-  OnKey3() {
-    this.showImage = this.imageUrl;
-  }
-
-  OnKey4() {
-    this.showUrl = this.urlText;
-  }
-
-  OnClick() {
-    this.showBgColor = this.bgColor;
-  }
+  bgColors:boolean=false; 
 }
