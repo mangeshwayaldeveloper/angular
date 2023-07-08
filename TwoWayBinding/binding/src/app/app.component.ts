b4a74b0f864d06aeb2acd197885e98758a0056fe
My name <me@example.com>
2023-07-08 21:02:21 +0530

    task 1 of angular

--- TwoWayBinding/binding/src/app/app.component.ts
+++ TwoWayBinding/binding/src/app/app.component.ts
@@ -9,9 +9,17 @@ export class AppComponent {
   title = 'binding';
   username: string = "";
   message: string = "";
+  textValue: string = "Value is comming from ts file";
+  textValue2:string="This is two way binding";
 
   onKeyUp() {
     console.log(this.username)
     this.message = "Hello " + this.username;
+  }
+  onKeyUpT(){
+    console.log(this.textValue)
+  }
+  onKeyUpTs(){
+    console.log(this.textValue2)
   }
 }
